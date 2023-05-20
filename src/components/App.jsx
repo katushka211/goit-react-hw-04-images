6e1c7dd89d41849b7de4670cdcc84241cfefda42
Kateryna Verveda <katushka211@gmail.com>
2023-05-20 19:40:17 +0200

    add hooks in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,41 @@
 import { GlobalStyle } from './GlobalStyle';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Searchbar } from './SearchBar/SearchBar';
 import { ToastContainer } from 'react-toastify';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Layout } from './Layout/Layout';
 
-export class App extends Component {
-  state = {
-    imageName: '',
+export const App = () => {
+  const [imageName, setImageName] = useState('');
+
+  const handleFormSubmit = imageName => {
+    setImageName(imageName);
   };
 
-  handleFormSubmit = imageName => this.setState({ imageName });
-  render() {
-    return (
-      <Layout>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery image={this.state.imageName} />
-        <ToastContainer autoClose={2500} />
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery image={imageName} />
+      <ToastContainer autoClose={2500} />
+      <GlobalStyle />
+    </Layout>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     imageName: '',
+//   };
+
+//   handleFormSubmit = imageName => this.setState({ imageName });
+//   render() {
+//     return (
+//       <Layout>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery image={this.state.imageName} />
+//         <ToastContainer autoClose={2500} />
+//         <GlobalStyle />
+//       </Layout>
+//     );
+//   }
+// }
