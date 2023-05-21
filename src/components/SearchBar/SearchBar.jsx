c235c71e1567d27923a565c36992d0e44d507cf0
Kateryna Verveda <katushka211@gmail.com>
2023-05-21 15:28:47 +0200

    Refactor SearchBar to React hooks

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import { SearchBar } from './SearchBar.styled';
@@ -6,49 +6,90 @@ import { SearchButton } from './SearchBar.styled';
 import { SearchBtnLabel } from './SearchBar.styled';
 import { FiSearch } from 'react-icons/fi';
 import { SearchFormInput } from './SearchBar.styled';
-
 import { SearchForm } from './SearchBar.styled';
-export class Searchbar extends Component {
-  state = {
-    imageName: '',
+
+export const Searchbar = ({ onSubmit }) => {
+  const [imageName, setImageName] = useState('');
+
+  const handleNameChange = event => {
+    setImageName(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ imageName: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.imageName.trim() === '') {
+    if (imageName.trim() === '') {
       toast.error('Please, enter something');
       return;
     }
-    this.props.onSubmit(this.state.imageName);
-    this.setState(this.setState({ imageName: '' }));
+    onSubmit(imageName);
+    setImageName('');
   };
 
-  render() {
-    return (
-      <SearchBar>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchButton type="submit">
-            <FiSearch size="20" />
-            <SearchBtnLabel />
-          </SearchButton>
-          <SearchFormInput
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.imageName}
-            onChange={this.handleNameChange}
-          />
-        </SearchForm>
-      </SearchBar>
-    );
-  }
-}
+  return (
+    <SearchBar>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchButton type="submit">
+          <FiSearch size="20" />
+          <SearchBtnLabel />
+        </SearchButton>
+        <SearchFormInput
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={imageName}
+          onChange={handleNameChange}
+        />
+      </SearchForm>
+    </SearchBar>
+  );
+};
 
 Searchbar.propType = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// export class Searchbar extends Component {
+//   state = {
+//     imageName: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ imageName: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.imageName.trim() === '') {
+//       toast.error('Please, enter something');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.imageName);
+//     this.setState(this.setState({ imageName: '' }));
+//   };
+
+//   render() {
+//     return (
+//       <SearchBar>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <SearchButton type="submit">
+//             <FiSearch size="20" />
+//             <SearchBtnLabel />
+//           </SearchButton>
+//           <SearchFormInput
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.imageName}
+//             onChange={this.handleNameChange}
+//           />
+//         </SearchForm>
+//       </SearchBar>
+//     );
+//   }
+// }
+
+// Searchbar.propType = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
